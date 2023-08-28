22b1cfefb2d788a8e0ea0c8db5411e5b54e03ab8
abdulsaif0511 <82303862+abdulsaif0511@users.noreply.github.com>
2023-08-28 17:21:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size (500,500);
@@ -5,13 +10,20 @@ function setup(){
     canvas = createCanvas(500,500);
     canvas.position(560,150);
 
-    poseNet = mle.poseNet(video, modeloaded);
+    poseNet = ml5.poseNet(video, modeloaded);
     poseNet.on('pose', gotPoses);
+
 }
 
-function gotPoses(){
+function gotPoses(results){
     if(results.length>0){
         console.log(results);
+       
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = leftWristX - rightWristX;
+        console.log("leftWritX =" +leftWristX + "rightWristX = " +rightWristX + "difference = " + difference );
     }
 }
 
@@ -21,4 +33,11 @@ function modeloaded(){
 
 function draw(){
     background('#969A97');
+
+    document.getElementById('font_size').innerhtml = "Width And Height of aSquare willbe =" +difference+"px";
+    fill('#F90093');
+    textSize(difference);
+    text('Abdul Saif',50,400);
 }
+
+
